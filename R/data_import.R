e65a1bf6e3972ae6e3bd67d39dc47b708df4a29e
Joy Zhou <zhou1559@umn.edu>
2023-05-01 18:12:38 -0500

    Cleaned, merged and saved datasets

--- R/data_import.R
+++ R/data_import.R
@@ -6,3 +6,14 @@ review_tbl <- read_delim("../data/satisfaction_reviews.csv", delim = ".", col_na
 dataset_tbl <- read_delim("../data/dataset.csv", delim = "+")
 
 colnames(review_tbl) <- c("satisfaction", "dissatisfaction", "employee_id")
+
+review_tbl_ordered <- review_tbl %>%
+  arrange(employee_id)
+
+dataset_tbl_num <- dataset_tbl %>%
+  mutate(employee_id = 1:nrow(dataset_tbl))
+
+ion_final_tbl <- dataset_tbl_num %>%
+  left_join(review_tbl_ordered, by = "employee_id")
+
+write_csv(ion_final_tbl, "../data/ion_final_tbl.csv")
