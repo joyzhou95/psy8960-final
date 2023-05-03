d156966880a724b2c2c58f4b1bde7eb92952b51e
Joy Zhou <zhou1559@umn.edu>
2023-05-03 11:35:55 -0500

    Finished analyses of H2

--- R/statistical.R
+++ R/statistical.R
@@ -11,14 +11,30 @@ ion_tbl <- read_csv("../data/ion_final_tbl.csv")
 pay_perf_cor <- ion_tbl %>%
   cor_test(vars = c("MonthlyIncome", "PerformanceRating"))
 
+## Test of H2
+pay_depart_anova <- ion_tbl %>%
+  anova_test(
+    formula = MonthlyIncome ~ Department,
+  )
+
 # Visualization 
 ## Visualization of H1 
 (ggplot(ion_tbl, aes(MonthlyIncome, PerformanceRating)) +
     geom_point(position = "jitter") + 
     geom_smooth(method = "lm", se = F) + 
     labs(x = "Monthly Income", y = "Performance Ratings", 
-         title = "The Relationship Between Monthly Pay and Performance Ratings ")) %>%
+         title = "The Relationship Between Monthly Pay and Performance Ratings") + 
+  theme(plot.title = element_text(hjust = 0.5))) %>%
   ggsave(filename = "../figs/H1.png", units = "px", width = 1920, height = 1080)
+
+## Visualization of H2
+(ggplot(ion_tbl, aes( MonthlyIncome, Department)) + 
+  geom_boxplot() + 
+  coord_flip() + 
+  labs(x = "Monthly Income", y = "Departments", 
+       title = "Monthly Pay by Departments") + 
+  theme(plot.title = element_text(hjust = 0.5))) %>%
+  ggsave(filename = "../figs/H2.png", units = "px", width = 1920, height = 1080)
 
 # Publication
 ## Publication Results for H1 
