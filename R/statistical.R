4adb1656432a2cdd440fe470c67f4871db527705
Joy Zhou <zhou1559@umn.edu>
2023-05-03 10:21:36 -0500

    Finished analyses for H1

--- R/statistical.R
+++ R/statistical.R
@@ -8,3 +8,18 @@ ion_tbl <- read_csv("../data/ion_final_tbl.csv")
 
 # Analysis 
 ## Test of H1
+pay_perf_cor <- ion_tbl %>%
+  cor_test(vars = c("MonthlyIncome", "PerformanceRating"))
+
+# Visualization 
+## Visualization of H1 
+(ggplot(ion_tbl, aes(MonthlyIncome, PerformanceRating)) +
+    geom_point(position = "jitter") + 
+    geom_smooth(method = "lm", se = F) + 
+    labs(x = "Monthly Income", y = "Performance Ratings", 
+         title = "The Relationship Between Monthly Pay and Performance Ratings ")) %>%
+  ggsave(filename = "../figs/H1.png", units = "px", width = 1920, height = 1080)
+
+# Publication
+## Publication Results for H1 
+
