13e60d775a2916c85da46414c902c1c176073a22
Joy Zhou <zhou1559@umn.edu>
2023-05-07 15:50:54 -0500

    Set up the input

--- shiny/app.R
+++ shiny/app.R
@@ -3,28 +3,45 @@ library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+  
   # Application title & author name
   titlePanel("PSY 8960 Final Week Shiny Project: Joy Zhou"),
-
+  
   # Sidebar with the select inputs
   sidebarLayout(
     #Set select inputs to be in the sidebar
     sidebarPanel(
       #Set up selectInput for gender, errorband, and competion time to allow users to filter their data based on their selections
       selectInput("variables", "Select the variable that you want to examine",
-                  choices = c("Monthly pay", 
-                              "Turnover status", 
-                              "Overall job satisfaction")),
-      selectInput("department", "Select the department that you want to examine", 
-                  choices = c("Human Resources", 
+                  selected = "None",
+                  choices = c("None",
+                              "MonthlyIncome", 
+                              "Attrition", 
+                              "JobSatisfaction")),
+      selectInput("department", "Select the department that you want to examine",
+                  selected = "All",
+                  choices = c("All",
+                              "Human Resources", 
                               "Research & Development", 
                               "Sales")),
+      selectInput("education", "Select the education field that you want to examine",
+                  selected = "All",
+                  choices = c("All",
+                              "Human Resources", 
+                              "Life Sciences", 
+                              "Marketing", 
+                              "Medical", 
+                              "Technical Degree",
+                              "Other")),
       selectInput("gender", "Select the gender that you want to examine", 
-                  choices = c("Male",
+                  selected = "All",
+                  choices = c("All",
+                              "Male",
                               "Female")),
-      selectInput("job role", "Select the job role that you want to examine", 
-                  choices = c("Healthcare Representative",
+      selectInput("job_role", "Select the job role that you want to examine", 
+                  selected = "All",
+                  choices = c("All",
+                              "Healthcare Representative",
                               "Human Resources", 
                               "Laboratory Technician", 
                               "Manager", 
@@ -44,22 +61,15 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$fig <- renderPlot({
-      # Import the saved RDS dataset that is appropriate for shiny, did not need to specify path as the current wd is the same as the wd of app.R
-      ion_skiny_tbl <- read_rds('./ion_tbl.RDS')
-      
-      
-    })
-}
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
 
 # Load library for deploying app
-library(rsconenct)
+#library(rsconnect)
+#rsconnect::setAccountInfo(name='joyzhou', token='2590A8DB5F2C12C2946B93E305228F9D', secret='Fr2jmw3fmOMtVIOpKGiVsENgS+9a0TTG/dOe+ll9')
 
 # Deploy and name the app 
-rsconnect::deployApp(appName = "shinny_week8")
+#rsconnect::deployApp(appName = "people_dashboard")
