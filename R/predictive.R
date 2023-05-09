6ad86f64c6dfc6fab5253aac00b024032f0041d8
Joy Zhou <zhou1559@umn.edu>
2023-05-09 10:57:04 -0500

    Edited codes and output of predictive.R

--- R/predictive.R
+++ R/predictive.R
@@ -19,9 +19,11 @@ ion_tbl <- read_csv("../data/ion_final_tbl.csv")
 
 ## Conduct natural language processing (NLP) analyses to convert reviews into variables that could be used in machine learning models
 
-## Drop NA values in the review columns and create a dataset containing only the reviews 
+ 
 ion_reviews <- ion_tbl %>%
+  ## Drop NA values in the review columns
   drop_na(satisfaction_txt,dissatisfaction_txt) %>%
+  ## Create a dataset containing only the reviews 
   select(satisfaction_txt, dissatisfaction_txt, employee_id)
 
 
@@ -120,13 +122,13 @@ ion_slim_dtm_dis <- removeSparseTerms(ion_dtm_dis, .96)
 
 ion_slim_matrix_dis <- as_tibble(as.matrix(ion_slim_dtm_dis))
 
-
 ion_slim_matrix_dis %>%
   summarise_if(is.numeric, var)
 
 colnames(ion_slim_matrix_dis) <- paste0(colnames(ion_slim_matrix_dis), "-dis")
 
 ion_reviews_nlp <- bind_cols(ion_reviews_satis, ion_slim_matrix_dis)
+
 
 
 ## Append all the text variables to the original master dataset
@@ -141,11 +143,11 @@ ion_tbl_com <- ion_tbl %>%
 variance <- ion_tbl_com %>%
   summarise_if(is.numeric, var)
 
-## Removed variables that had zero variance, as they would have zero predictive validity of the outcome
+## Remove variables that had zero variance, as they would have zero predictive validity of the outcome
 ion_tbl_final <- ion_tbl_com %>%
   select(-EmployeeCount, -Over18, -StandardHours)
 
-## Create a dataset that does not contain any of the text variables
+## Create a separate dataset that does not contain any of the text variables for later analyses 
 ion_tbl_num <- ion_tbl_final %>%
   select(!ends_with(c("satis", "dis")))
 
@@ -185,10 +187,10 @@ toc_model_glm <- toc()
 model_glm
 
 ## Get the predicted values from the model
-p_glm <- predict(model_glm, ion_test_tbl, na.action=na.pass)
+p_glm <- predict(model_glm, ion_test_tbl, na.action = na.pass)
 
 ## Obtain statistics of accuracy, specificity and sensitivity
-glm_test_acc <- confusionMatrix(p_glm, ion_test_tbl[["Attrition"]])
+glm_test_acc <- confusionMatrix(p_glm, as.factor(ion_test_tbl[["Attrition"]]))
 
 ## Follow the same steps above for building the other two models 
 tic()
@@ -203,9 +205,9 @@ model_rf <- train(
 toc_model_rf <- toc()
 model_rf
 
-p_rf <- predict(model_rf, ion_test_tbl, na.action=na.pass)
+p_rf <- predict(model_rf, ion_test_tbl, na.action = na.pass)
 
-rf_test_acc <- confusionMatrix(p_rf, ion_test_tbl[["Attrition"]])
+rf_test_acc <- confusionMatrix(p_rf, as.factor(ion_test_tbl[["Attrition"]]))
 
 tic()
 model_xgb <- train(
@@ -221,7 +223,7 @@ model_xgb
 
 p_xgb <- predict(model_xgb, ion_test_tbl, na.action=na.pass)
 
-xgb_test_acc <- confusionMatrix(p_xgb, ion_test_tbl[["Attrition"]])
+xgb_test_acc <- confusionMatrix(p_xgb, as.factor(ion_test_tbl[["Attrition"]]))
 
 ## Compare the performance of the three models 
 summary(resamples(list(model_glm, model_rf, model_xgb)))
@@ -230,7 +232,9 @@ dotplot(resamples(list(model_glm, model_rf, model_xgb)), metric = "Accuracy")
 
 
 ## Ended up choosing the elastic net model as it has higher sensitivity, specificity and accuracy in the test set 
-## as well as shorter computation time compared to the other two models
+## as well as shorter computation time compared to the other two models. Especially given that our goal for this 
+## project is to maximize predictive validity in new samples, I decided to go with the elastic net model. 
+
 
 ## Follow the same steps above to build the elastic net model without text data 
 train_cases_num <- sample(1:nrow(ion_tbl_num), .75*nrow(ion_tbl_num))
@@ -253,7 +257,7 @@ model_glm_num
 
 p_glm_num <- predict(model_glm_num, ion_num_test_tbl, na.action=na.pass)
 
-glm_num_test_acc <- confusionMatrix(p_glm_num, ion_num_test_tbl[["Attrition"]])
+glm_num_test_acc <- confusionMatrix(p_glm_num, as.factor(ion_num_test_tbl[["Attrition"]]))
 
 ## Stop parallel processing
 stopCluster(local_cluster)
@@ -269,9 +273,9 @@ dotplot(resamples(list(model_glm, model_glm_num)), metrix = "Accuracy")
 ## sensitivity, and computation time
 model_comp_tbl <- tibble(
   Models = c("glmnet","ranger","xgbTree"),
-  cv_accuracy = str_remove(round(
-    resample_sum$statistics$Accuracy[,"Mean"],2
-  ),"^0"),
+  cv_accuracy = str_remove(format(
+    round(resample_sum$statistics$Accuracy[,"Mean"],2), nsmall = 2), 
+  "^0"),
   ho_accuracy = str_remove(c(
     format(round(glm_test_acc$overall[1],2),nsmall=2),
     format(round(rf_test_acc$overall[1],2),nsmall=2),
@@ -302,8 +306,8 @@ write_csv(model_comp_tbl, "../out/Model Comparison Table.csv")
 ## What characteristics of how you created the final model likely made the biggest impact in maximizing its performance? How do you know? 
 
 ## I think the choice of model building that had the largest impact on the its performance is the selection of the modeling method.
-## Looking at the model comparison table, the elastic net model had the highest accuracy for predicting the test sample (.91) and the lowest accuracy
-## for predicting the training sample (.89) compared to the other two models, and it also has the highest specificity (.99) and sensitivity (.40). 
+## Looking at the model comparison table, the elastic net model had the highest accuracy for predicting the test sample (.90) and the lowest accuracy
+## for predicting the training sample (.88) compared to the other two models, and it also has the highest specificity (.49) and sensitivity (.97). 
 ## These results are likely due to elastic net model's balance between the lasso and ridge penalty, which penalizes model complexity, reduces overfitting 
 ## and are good at handling multicollinearity, which is likely an issue in the current dataset as many of the variables are correlated with each other 
 ## (e.g., age and job level, monthly rate and monlyth income). Additionally, its penalty for model complexity allows the elastic net model to outperform
@@ -337,10 +341,10 @@ write_csv(text_comp_tbl, "../out/Text Data Comparison Table.csv")
 ### Question: 
 ## What is the incremental predictive accuracy gained by including text data in your model versus not including text data?
 
-## Including text data in the model increased specificity and predictive accuracy in the test set, yet the model performed
-## worse in terms of predictive accuracy in the train set and sensitivity, which suggests that the text data probably added some 
-## noise in the model so that it performs better in out-of-sample predictions yet worse with the training sample compared to the 
-## model without text data. With all those being said, the difference between the two models is quite small. 
+## Including text data in the model increased specificity, sensitivity, and predictive accuracy in the test set, which suggests that the text data 
+## probably added some noise in the model so that it performs better in out-of-sample predictions.
+## However, the difference between the two models is quite small, and rerunning the models yielded quite different results,
+## so it is hard to draw a firm conclusion regarding the incremental predictive validity of text data. 
 
 
 
